76e3976129ba5a76418f495741cb0df0fb4c3a68
Jed Rembold <jedediah.r@gmail.com>
2020-09-25 11:54:40 -0700

    Add initial template and readme

---
+++ Lab10.py
@@ -0,0 +1,24 @@
+# File: Lab10.py
+
+"""
+Program to check to see if a sentence or phrase
+is a palindrome if punctuation, spacing, and
+capitalization is ignored.
+"""
+
+def is_simple_palindrome():
+    pass
+
+
+
+def simplify_string():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    string = input('Enter your desired string: ')
+    simple_string = simplify_string(string)
+    print("The simplified string is:", simple_string)
+    print("It is", "not"*(not is_simple_palindrome(simple_string)), "a palindrome!")
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Lab10
-Materials for Lab 10
+# Lab 10
+
+In this lab you'll look to write a few functions to help you determine if a particular sentence or phrase is a palindrome if punctuation, whitespace, and capitalization is ignored.
+
+I do have autotests running for this program, so you'll be able to be sure things are working properly!
